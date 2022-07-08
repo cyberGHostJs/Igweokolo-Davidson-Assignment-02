4a01ab9d619b885aa112e9ba081682db4142b276
Igweokolo Davidson Onyebuchi <105425922+cyberGHostJs@users.noreply.github.com>
2022-07-08 15:44:02 +0000

    challenge 1 and challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -1,4 +1,4 @@
-// ========================
+
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
 // ========================
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let sum = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+  sum += arrayOfNumbers[i]
+  }
+    return sum;
+};
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+  for (let i = 0; i < arrayOfNumbers.length; i++){}
+  let even = arrayOfNumbers.filter (number => number %2 ==0)
+  
+return even.length
+} 
+
+
+assignment.countEvenNumbers = countEvenNumbers;
+
+
 
 /**
  * Challenge - 3
@@ -55,5 +67,4 @@ function celsiusToFahrenheit(arrayOfNumbers) {
 // ========================
 // DO NOT EDIT THIS BLOCK
 module.exports = assignment;
-// ========================
-
+// ========================//
